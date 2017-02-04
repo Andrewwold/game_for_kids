8ed165676db0a4c86931539c95d5af6acc5bbc0e
Andrew <goldsabre@gmail.com>
2017-02-04 15:44:04 -0700

    added music and fixed glitches

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,9 +27,36 @@ $(document).ready(function(){
 });
 
 $(function() {
-  var colors = ["#FC3E6B","#24FF00","#0087F9","#F9F900"];
+  var colors = ["#BA55D3","#9370DB","#8A2BE2","#9400D3","#9932CC","	#8B008B","#800080"];
+  var colors2 = ["#BA55D3","#9370DB","#8A2BE2","#9400D3","#9932CC","#8B008B","#800080"];
+  var colors3 = ["#BA55D3","#9370DB","#8A2BE2","#9400D3","#9932CC","#8B008B","#800080"];
+  var colors4 = ["#BA55D3","#9370DB","#8A2BE2","#9400D3","#9932CC","#8B008B","#800080"];
 
   $('.arrow-left').hover(
+    function() {
+      var rand = Math.floor(Math.random()*colors.length);
+      var rand2 = Math.floor(Math.random()*colors2.length);
+      var rand3 = Math.floor(Math.random()*colors2.length);
+      var rand4 = Math.floor(Math.random()*colors2.length);
+      $(this).css('width', '0px');
+      $(this).css('height', '0px');
+      $(this).css('border-style', 'solid');
+      $(this).css('border-width', '5px');
+      $(this).css('border-top-color', colors[rand]);
+      $(this).css('border-right-color', colors2[rand2]);
+      $(this).css('border-bottom-color', colors3[rand3]);
+      $(this).css('border-left-color', colors4[rand4]);
+    },
+    function () {
+    	$(this).removeAttr('style');
+    }
+  );
+});
+
+$(function() {
+  var colors = ["#FC3E6B","#24FF00","#0087F9","#F9F900"];
+
+  $('.arrow-left').focus(
     function() {
       var rand = Math.floor(Math.random()*colors.length);
       $(this).css('width', '0px');
@@ -43,3 +70,4 @@ $(function() {
     }
   );
 });
+
